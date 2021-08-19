d86d00789eac38bf2cefc3272b96edf156673d21
Dylan McCullor <dmccullor0@gmail.com>
2021-08-19 00:15:32 -0500

    Adding completed proejct

--- scraping.py
+++ scraping.py
@@ -28,7 +28,7 @@ def scrape_all():
 def mars_news(browser):
     #Scrape Mars News
     #Visit the mars nasa news site
-    url = 'https://redplanetscience.com'
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
     browser.visit(url)
     #Optional delay for loading the page
     browser.is_element_present_by_css('div.list_text', wait_time=1)
@@ -51,7 +51,7 @@ def mars_news(browser):
 
 def featured_image(browser):
     #Visit URL
-    url = 'https://spaceimages-mars.com'
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     browser.visit(url)
 
     #find and click the full image button
@@ -64,24 +64,24 @@ def featured_image(browser):
 
     #Find the relative image url
     try:
-        img_url_rel = img_soup.find('img', class_='thumbimg').get('src')
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
     except AttributeError:
         return None
 
     #Use the base URL to create an absolute URL
-    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
     return img_url
 
 def mars_facts():
     try:
         #Use 'read_html' to scrape the table into a dataframe
-        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
     except BaseException:
         return None
 
     #Assign columns and set index of dataframe
-    df.columns=['description', 'Mars', 'Earth']
-    df.set_index('description', inplace=True)
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
 #Convert dataframe to html format, add bootstrap
     return df.to_html(classes= "table table-striped")
