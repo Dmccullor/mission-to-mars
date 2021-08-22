be951e9e10bf10d7bb273d2838b139aa233281ad
dmccullor <dmccullor0@gmail.com>
2021-08-22 12:56:23 -0500

    deliverable 2 complete

--- scraping.py
+++ scraping.py
@@ -17,6 +17,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": hemispheres(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -85,6 +86,27 @@ def mars_facts():
 
 #Convert dataframe to html format, add bootstrap
     return df.to_html(classes= "table table-striped")
+
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    hemisphere_image_urls = []
+
+    try:
+       for i in range(4):
+            hemispheres = {}
+            browser.find_by_css('a.product-item h3')[i].click()
+            element = browser.links.find_by_text('Sample').first
+            img_url = element['href']
+            title = browser.find_by_css("h2.title").text
+            hemispheres["img_url"] = img_url
+            hemispheres["title"] = title
+            hemisphere_image_urls.append(hemispheres)
+            browser.visit(url)
+    except BaseException:
+        return None
+    return hemisphere_image_urls
+        
 
 if __name__ == "__main__":
     #If running as script, print scraped data
